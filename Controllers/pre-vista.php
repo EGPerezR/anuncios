f3410012307df9d3169946eeee98e4585f68095c
EGPerezR <19170161@uttcampus.edu.mx>
2022-06-06 16:11:58 -0500

    otro

--- Controllers/pre-vista.php
+++ Controllers/pre-vista.php
@@ -1,6 +1,6 @@
 <?php
 require 'conexion.php';
-
+$ruta = "../img/";
 $material = "SELECT * FROM image, messages";
 
 $ejecutra = mysqli_query($mysql, $material);
@@ -20,33 +20,22 @@ if ($ejecutra-> num_rows > 0){
     while($row = $ejecutra->fetch_assoc()){
         echo "<tr>";
             echo "<td>";
+                echo "<img src'".$ruta.$row['name']."'>";
+            echo "</td>";
+        echo "<tr>";
     }
+}else{
+    echo "nada";
 }
 
 
 ?>
 </table>
 <?php
-require 'conexion.php';
-
-$material = "SELECT * FROM image, messages";
-
-$ejecutra = mysqli_query($mysql, $material);
 ?>
-<table>
-<tr>
-    <th>
-    </th>
-</tr>
-
 
 <?php
-if ($ejecutra-> num_rows > 0){
 
-    while($row = $ejecutra->fetch_assoc()){
-
-    }
-}
 
 
 ?>
